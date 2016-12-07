a4fd78e1128f2002c72fb7de4fd9bf0a2d33dc13
william-billaud <billaudwilliam@gmail.com>
2016-12-07 12:06:10 +0100

    Ajout methode de verification+modifications gitignore (#6)
    * regexp telephone
    
    ajout de la verification de la conformite du numero de telephone
    
    * modif telephone
    
    correction oublie
    
    * ajout commentaire et documentation
    
    * modification gitignore
    
    * ajout methode verification numero de telephone, email et mot de passe dans la classe model

--- Leasen/class/Model.php
+++ Leasen/class/Model.php
@@ -50,4 +50,56 @@ class Model
         }
 
     }
+
+
+    /**
+     * @param string $mail adresse mail a verifier
+     * @return bool true si l'adresse est valide, false si non
+     */
+    public function est_valide_mail($mail)
+    {
+        $regexp_mail="/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@]isen.yncrea.fr$/";
+        if(isset($mail))
+        {
+            if(preg_match($regexp_mail,$mail))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param string $telephone numero de telelephone a verifier
+     * @return bool true si il est valide, false si non
+     */
+    public function est_valide_telephone($telephone)
+    {
+        $regexp_telephone="/^([+]([1-9]){1,3}|0)[1-79]([-. ]?[0-9]){8}$/";
+        if(isset($telephone))
+        {
+            if(preg_match($regexp_telephone,$telephone))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param $mot_de_passe mot_de_passe a verifier
+     * @return bool true si il contient au moins  caractère, majuscule, une minuscule et un chiffre
+     */
+    public function est_valide_mot_de_passe($mot_de_passe)
+    {
+        $regexp_mot_de_passe ="/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/";
+        if(isset($mot_de_passe))
+        {
+            if(preg_match($regexp_mot_de_passe,$mot_de_passe))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
