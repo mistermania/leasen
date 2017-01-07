be6f5ffc21902f3c0c573863771ecd151da449a6
mistermania <misterplaymania@gmail.com>
2017-01-07 20:23:27 +0100

    gre
    grege

---
+++ Leasen/class/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: billaud
+ * Date: 05/12/16
+ * Time: 20:42
+ */
+class Config
+{
+    /**
+     * @var int 1 pour afficher les informations de debug,0 sinon
+     */
+    static $debug =1;
+    /**
+     * @var array contient les informatons concernant la BDD
+     */
+    public static $config = array( "HOST" => 'localhost',
+                                "USER" => "leasen",
+                                "PASSWORD" => "root",
+                                "DB_NAME" => "db_leasen");
+
+}
