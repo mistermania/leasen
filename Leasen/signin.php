718d5ccc19b628422e7b65496414672378ea7cee
mistermania <misterplaymania@gmail.com>
2017-01-07 20:22:37 +0100

    signin/signup
    ajout page creation de compte + page de connexion

---
+++ Leasen/signin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+            require('class/Autoloader.php');
+            Autoloader::register();
+        ?>
+        <?php
+            $newuser = new Utilisateur();
+            $e_mail =   filter_input(INPUT_POST, 'user_email');
+            $mot_de_passe = filter_input(INPUT_POST, 'pass');
+            
+            
+            $test= $newuser->find("e_mail = '$e_mail'");
+           echo '<pre>';
+            $hash=$test[0]['hash_mot_de_passe'];
+            if(!empty($test)) 
+            {
+                if(password_verify($mot_de_passe, $hash))
+                {
+                    echo ' c\'est bon jamy';
+                }else
+                {
+                    echo 'fail';
+                }
+            }
+            
+            echo '</pre>';
+        ?>
+    </body>
+</html>
