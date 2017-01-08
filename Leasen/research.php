810a0e1255167b2827c6ff8e89247517b0b567a5
billaud <billaudwilliam@gmail.com>
2017-01-08 11:52:28 +0100

    RECHERCHE
    ajout class recherche ( utilisatation d'une view)

--- Leasen/research.php
+++ Leasen/research.php
@@ -1,40 +1,16 @@
 <?php
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=leasen;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-
-$recherche = $_POST['recherche'];
-$categorie = $_POST['categorie'];
-$date = $_POST['date'];
-$sql = "SELECT * FROM objet WHERE nom LIKE '%" . $recherche.'%\'  AND categorie = \''.$categorie.'\' AND date = \''.$date.'\'';
-$obj = new Objet();
-$req = $bdd->query($sql);
-
+require('class/Autoloader.php');
+Autoloader::register();
+$info=array();
+$info['chaine'] = 'clette';
+$info['id_type'] = 2;
+$info['date_debut'] = '2017-12-15 23:54:36';
+$info['duree']=1;
+$test= new Recherche();
+$res=$test->effectueRecherche($info);
+print_r($res);
 ?>
 
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Categorie</th>
-        <th>Date</th>
-        <th>Nom</th>
-    </tr>
-    <?php
-    while ($donnees = $req->fetch()) {
-        ?>
-        <tr>
-            <td><?php echo $donnees['id']; ?> </td>
-            <td><?php echo $donnees['categorie']; ?> </td>
-            <td><?php echo $donnees['date']; ?> </td>
-            <td><?php echo $donnees['nom']; ?> </td>
-        </tr>
-        <?php
-    }
-   $req->closeCursor();
-    ?>
-</table>
 
 
 
