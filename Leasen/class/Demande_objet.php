4d0cbae03a528f140898582b41795340b54de7bc
billaud <billaudwilliam@gmail.com>
2017-01-19 18:06:53 +0100

    AJout insert type & demande_objet

--- Leasen/class/Demande_objet.php
+++ Leasen/class/Demande_objet.php
@@ -9,5 +9,32 @@ class demande_objet extends Model
 {
 	const champ=array('date_demande_objet','description_objet','titre_demande',id_utilisateur,id_type);
 
+/**
+*@param $i : tableau contenant les champs a inserer
+* @return 1 : id_utilisateur, description_objet ou titre_demande non renseignée
+*@return 2 :id_utilisateur absent de la Bdd
+*@return 3 : id_type absent de la Bdd
+*@return 7 : clé incorrecte dans le tableau
+*/
+	public function insert($i)
+	{
+		if(!isset($i['id_utilisateur']) || !isset($i['description_objet']) || !isset(['titre_demande']))
+		{
+			return 1;
+		}
+
+		$i['date_demande_objet']=date('Y-m-d H:i:s');
+		if(Model::idAbsent($i['id_utilisateur'],'Utilisateur'))
+		{
+			return 2;
+		}
+		if(Model::idAbsent($i['id_type'],'Type'))
+		{
+			return 3;
+		}
+
+		return $this->insertBdd($i);
+	}
+
 }
 ?>
--- Leasen/class/Type.php
+++ Leasen/class/Type.php
@@ -8,6 +8,19 @@
  */
 class Type extends Model
 {
-    const champ=array('description_type','id_type');
+    const champ=array('description_type');
 
+		/**
+		*@param $i tableau contenant les champs à inserer
+		*@return 1 : description_type non defini
+		*@return 7 : clé incorrecte dans le tableau
+		*/
+		public function FunctionName($i)
+		{
+			if(!isset($i['description_type']))
+			{
+				return 1;
+			}
+			return $this->insertBdd($i);
+		}
 }
