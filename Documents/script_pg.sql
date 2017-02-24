f60a9db92f9d1875ea2c2d99f1eb97372127e123
Unknown <aubois.alexandre@gmail.com>
2017-02-24 20:58:45 +0100

    Erreur update statut location

--- Documents/script_pg.sql
+++ Documents/script_pg.sql
@@ -166,7 +166,7 @@ FROM location INNER JOIN objet ON location.id_objet=objet.id_objet;
 
 CREATE VIEW recherche_message 
 AS SELECT objet.id_objet, objet.id_utilisateur, objet.nom_objet, 
-location.statut_location, location.date_debut, location.date_fin, location.id_utilisateur AS id_loueur,
+location.statut_location, location.date_debut, location.date_fin, location.id_utilisateur AS id_loueur, location.id_location,
 utilisateur.nom
 FROM objet INNER JOIN location ON objet.id_objet=location.id_objet
 INNER JOIN utilisateur ON objet.id_utilisateur=utilisateur.id_utilisateur;
--- Leasen/pages/confirmer_location.php
+++ Leasen/pages/confirmer_location.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 require('../class/Autoloader.php');
 Autoloader::register(1);
@@ -6,22 +7,40 @@ Autoloader::register(1);
 $idobjet = filter_input(INPUT_POST, 'id_objet');
 $newLocation = new Location();
 
-$locationInfos['id_objet'] = filter_input(INPUT_POST, 'id_objet');
+$locationInfos['id_location'] = filter_input(INPUT_POST, 'id_location');
 $res = $newLocation->find($locationInfos);
 
 foreach ($res as $k => $v) {
-    echo $v['id_location'];
-    echo $v['date_debut'];
-    echo $v['date_fin'];
-    echo $v['statut_location'];
-    echo $v['date_demande'];
-    echo $v['id_utilisateur'];
-    echo $v['id_objet'];
+    echo 'id location :'.$v['id_location'].'<br>';
+    $locationModif['id_location'] = $v['id_location'];
+    
+    echo 'date debut'.$v['date_debut'].'<br>';
+    $locationModif['date_debut'] = $v['date_debut'];
+    
+    echo 'date fin'.$v['date_fin'].'<br>';
+    $locationModif['date_fin'] = $v['date_fin'];
+    
+    echo 'statut location'.$v['statut_location'].'<br>';
+    $locationModif['statut_location'] = $v['statut_location'];
+    
+    echo 'date demande'.$v['date_demande'].'<br>';
+    $locationModif['date_demande'] = $v['date_demande'];
+    
+    echo 'id utilisateur'.$v['id_utilisateur'].'<br>';
+    $locationModif['id_utilisateur'] = $v['id_utilisateur'];
+    
+    echo 'id objet'.$v['id_objet'].'<br>';
+    $locationModif['id_objet'] = $v['id_objet'];
 }
 
 
-$locationModif['statut_location'] = '2';
+$locationModif['statut_location'] = 2;
+
+
+echo 'new statut :'.$locationModif['statut_location'];
 
 $modif = $newLocation->update($locationModif, $idobjet);
+
+echo '<br> retour fonction'.$modif;
 
 
