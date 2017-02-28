9dea450c1be048f8fe129bba4ab68ed25cb17c4a
Thoril <artru.thomas.monchal@gmail.com>
2017-02-28 22:16:53 +0100

    Ajout de l'image postobjet

--- Leasen/fonctions/newobjet.php
+++ Leasen/fonctions/newobjet.php
@@ -8,46 +8,35 @@ Autoloader::register(1);
 $maxsize = 1000000;
 $erreur = 0;
 $extensions_valides = array('jpg', 'jpeg', 'gif', 'png');
-print_r($_FILES);
-echo '</br>';
-/*if(isset($_FILES['image'])) {
-    echo $_FILES['image']['name'];
-    echo $_FILES['image']['type'];
-    echo $_FILES['image']['size'];
-    echo $_FILES['image']['error'];
-    echo $_FILES['image']['tmp_name'];
-}else {
-    echo "bonjour</br>";
-    php mailer
-}*/
-$name = $_FILES["image"]["name"];
+
 $exention_image = strtolower(substr(strchr($_FILES['image']['name'], '.'), 1));
-$uploads_dir = '../image/imgObjet';
+$name = $_FILES["image"]["name"];
+$uploads_dir = '/image';
+
 if ($_FILES['image']['error'] > 0) {
     $erreur = 1;
 }
 if ($_FILES['image']['size'] > $maxsize) {
     $erreur = 2;
 }
-$newobjet = new Objet();
 
 if (in_array($exention_image, $extensions_valides) && $erreur == 0) {
-    $nom = md5(uniqid(rand(), true));
-    $nomImage = "{$nom}.{$exention_image}";
-    if (move_uploaded_file($_FILES['image']['tmp_name'], "$uploads_dir/$name") == TRUE) {
-        $objetInfos['url_photo'] = $nomImage;
+    if (move_uploaded_file($_FILES['image']['tmp_name'], "../image/imgObj/$name") == TRUE) {
 
     }
+    $objetInfos['url_photo'] = "/image/imgObj/$name";
+    echo $objetInfos['url_photo'] . '</br> ';
 }
 
-echo $objetInfos['url_photo'] . '</br> ';
 $objetInfos['nom_objet'] = filter_input(INPUT_POST, 'nom');
 $objetInfos['description_objet'] = filter_input(INPUT_POST, 'description');
 $objetInfos['id_type'] = filter_input(INPUT_POST, 'categorie');
 $objetInfos['prix'] = filter_input(INPUT_POST, 'prix');
 $objetInfos['prix_caution'] = filter_input(INPUT_POST, 'prix_caution');
 $objetInfos['id_utilisateur'] = $_SESSION['IDUSER'];
-$test = $newobjet->insert($objetInfos);
-echo 'resultat ' . $test;
+
+$newobjet = new Objet();
+$res = $newobjet->insert($objetInfos);
+echo 'resultat ' . $res;
 
 //header('Location:../index.php');
