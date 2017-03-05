077af9a2335786f1ec9b752399da3ba3e93d1e14
Unknown <aubois.alexandre@gmail.com>
2017-03-05 09:38:33 +0100

    Redirection mauvaise inscription, connexion, envoyer demande

--- Leasen/pages/signin.php
+++ Leasen/pages/signin.php
@@ -5,36 +5,46 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
-<?php
-session_start();
-require('../class/Autoloader.php');
-Autoloader::register(1);
-$newuser = new Utilisateur();
-$e_mail = filter_input(INPUT_POST, 'user_email');
-$mot_de_passe = filter_input(INPUT_POST, 'pass');
-$test = $newuser->find(array('e_mail' => $e_mail));
-if (!empty($test)) {
-    $hash = $test[0]['hash_mot_de_passe'];
-    if (!empty($test)) {
-        if (password_verify($mot_de_passe, $hash)) {
-            echo 'Bonjour ' . $test[0]['nom'] . '';
-            $_SESSION['USER'] = $e_mail;
-            $_SESSION['IDUSER'] = $test[0]['id_utilisateur'];
-            header('Location:../index.php');
-            exit();
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        require('../class/Autoloader.php');
+        Autoloader::register(1);
+        $newuser = new Utilisateur();
+        $e_mail = filter_input(INPUT_POST, 'user_email');
+        $mot_de_passe = filter_input(INPUT_POST, 'pass');
+        $test = $newuser->find(array('e_mail' => $e_mail));
+        if (!empty($test)) {
+            $hash = $test[0]['hash_mot_de_passe'];
+            if (!empty($test)) {
+                if (password_verify($mot_de_passe, $hash)) {
+                    echo 'Bonjour ' . $test[0]['nom'] . '';
+                    $_SESSION['USER'] = $e_mail;
+                    $_SESSION['IDUSER'] = $test[0]['id_utilisateur'];
+                    header('Location:../index.php');
+                    exit();
+                } else {
+                    echo 'Mauvaise combinaison, merci de réessayer.';
+                    $retour=1;
+                }
+            }
         } else {
             echo 'Mauvaise combinaison, merci de réessayer.';
+            $retour=1;
         }
-    }
-} else {
-    echo 'Mauvaise combinaison, merci de réessayer.';
-}
-include "../fonctions/footer.php";
-?>
-</body>
+        ?>
+        <form name="redirect" action="../pages/connexion.php" method="post">
+            <input type="hidden" id="resu" name="resu" value="<?php echo $retour; ?>" >
+        </form>
+        <script type="text/javascript">
+            document.redirect.submit();
+        </script>
+        <?php
+        include "../fonctions/footer.php";
+        ?>
+    </body>
 </html>
--- Leasen/pages/signup.php
+++ Leasen/pages/signup.php
@@ -9,7 +9,7 @@ $mot_de_passe = filter_input(INPUT_POST, 'pass');
 $telephone = filter_input(INPUT_POST, 'numerotel');
 $partager_telephone = filter_input(INPUT_POST, 'telchoix');
 echo "$nom / $prenom / $e_mail / $mot_de_passe / $telephone / $partager_telephone <br/>";
-$userInfos = array('nom' => $nom, 'prenom' => $prenom, 'e_mail' => $e_mail, 'mot_de_passe' => $mot_de_passe, 'telephone' => $telephone, 'partager_telephone' => (int)$partager_telephone);
+$userInfos = array('nom' => $nom, 'prenom' => $prenom, 'e_mail' => $e_mail, 'mot_de_passe' => $mot_de_passe, 'telephone' => $telephone, 'partager_telephone' => (int) $partager_telephone);
 $test = $newuser->insert($userInfos);
 /**
  * @param $info array contenant : nom, prenom, email, partager_telephone, telephone,statut, mot de passe
@@ -21,27 +21,24 @@ $test = $newuser->insert($userInfos);
  * @return int 6 : numero deja present dans la base de donnée
  * @return int 0 : insertion reussie
  */
-if ($test == 1) {
-    echo "nom, prenom ou email absent";
-}
-if ($test == 2) {
-    echo "numero de téléphone non valide";
-}
-if ($test == 3) {
-    echo "adresse mail invalide";
-}
-if ($test == 4) {
-    echo "mot de passe trop faible (moins de 8 caractère, pas de chiffre ou/et pas de majuscules";
-}
-if ($test == 5) {
-    echo "Adresse déjà utilisée";
-}
-if ($test == 6) {
-    echo "numero de telephone déjà utilisé";
-}
+
+
 if ($test == 0) {
-    if (Config::$debug != 1) {
-        header('Location:../index.php');
-    }
+    ?>
+    <form name="redirect" action="../pages/connexion.php" method="post">
+        <input type="hidden" id="resu" name="resu" value="<?php echo $test; ?>" >
+    </form>
+    <?php
+} else {
+    ?>
+    <form name="redirect" action="../pages/inscription.php" method="post">
+        <input type="hidden" id="resu" name="resu" value="<?php echo $test; ?>" >
+    </form>
+    <?php
 }
 include "../fonctions/footer.php";
+?>
+
+<script type="text/javascript">
+    document.redirect.submit();
+</script>
